3767860ac689f7334353307333eef9b72972f019
kdmontero <kevindmontero@gmail.com>
2025-03-22 22:03:51 +0800

    added `Product` model

--- auto_quote/products/models.py
+++ auto_quote/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    description = models.TextField()
+    sell_price = models.IntegerField(default=0)
+    cost_price = models.IntegerField(default=0)
