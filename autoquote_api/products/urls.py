64ed20eefafb8afb06f0e0856902b932a9977a36
kdmontero <kevindmontero@gmail.com>
2025-04-12 20:41:40 +0800

    add product retrieve, update, destroy

--- autoquote_api/products/urls.py
+++ autoquote_api/products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import ProductListCreateView
+from .views import ProductRetrieveUpdateDestroyView
 
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductListCreateView.as_view())
+    path('', ProductListCreateView.as_view()),
+    path('<pk>/', ProductRetrieveUpdateDestroyView.as_view()),
 ]
--- autoquote_api/products/views.py
+++ autoquote_api/products/views.py
@@ -1,9 +1,18 @@
+from rest_framework import exceptions
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductListCreateView(ListCreateAPIView):
+    model = Product
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     model = Product
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
